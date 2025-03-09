ae00f5bb61531b15cca19153fc8590cd322d9012
Jorge Gabriel <jorge85x@gmail.com>
2025-03-09 19:24:59 -0300

    Add Two Numbers

---
+++ leetcode/2_AddTwoNumbers/ListNode.java
@@ -0,0 +1,45 @@
+
+// class Solution {
+
+//     class ListNode {
+//         int val;
+//         ListNode next;
+    
+//         ListNode() {}
+    
+//         ListNode(int val) {
+//             this.val = val;
+//         }
+    
+//         ListNode(int val, ListNode next) {
+//             this.val = val;
+//             this.next = next;
+//         }
+//     }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode nodeAux = new ListNode(0);
+        ListNode current = nodeAux;
+        
+        int rest = 0;
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            
+            int sum = x + y + rest;
+            rest = sum / 10;
+            
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        
+        if (rest > 0) {
+            current.next = new ListNode(rest);
+        }
+        
+        return nodeAux.next;
+    }
+}
